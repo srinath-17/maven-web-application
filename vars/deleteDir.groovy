774e716be06664a773b30a9660e6f2baddd87be7
srinath-17 <85933521+srinath-17@users.noreply.github.com>
2022-10-28 01:14:28 +0530

    Update deleteDir.groovy

--- vars/deleteDir.groovy
+++ vars/deleteDir.groovy
@@ -1,5 +1,4 @@
 def call(String deleteDir = sh " rm -rf target/maven-web-application.war "){
   
 }  
-def call( String CheckOutscm =  "git branch: 'development', url: 'https://github.com/srinath-17/maven-web-application.git'"){
-}
+
